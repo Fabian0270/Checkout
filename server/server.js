2d107cb681e9cf060d48529e5d5e9a48c3f4c5e7
fabian0270 <fabianolsson28@gmail.com>
2024-04-15 04:12:46 +0200

    Intial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const stripe = require("stripe")("sk_test_51P5cByGJMzMYUy5TK7ZHSDbqsnRrA4d2hLHYUED3YDumMc7eDo6tBVILKONhfSLznMmokHNV72bGRbPxrr6KIGfD00P5I7i61Q")
+
+const app = express()
+
+app.use(cors())
+
+app.get("/products", async (req, res) => {
+    const products = await stripe.products.list({
+        expand: ["data.default_price"]
+    });
+    res.status(200).json(products)
+})
+
+app.listen(3000, () => console.log("Server is up...."))
